058ac9e86ff57b8e8df5f71bc824ba40f7cda74b
Dorrian Weber <dorrianweber@gmail.com>
2021-02-25 22:24:10 -0800

    Wrote Intern test file; all tests passing

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -1,2 +1,37 @@
 // Importing class
 const Intern = require("../lib/Intern");
+
+describe("Intern class", () => {
+    describe("Initialization", () => {
+        it("should create an Intern with name, id, email, & school", () => {
+            // Act
+            const obj = new Intern("Dorrian", 12345, "dorrianweber@gmail.com", "UC Davis");
+    
+            // Assert
+            expect(obj.name).toEqual("Dorrian");
+            expect(obj.id).toEqual(12345);
+            expect(obj.email).toEqual("dorrianweber@gmail.com");
+            expect(obj.school).toEqual("UC Davis");
+        });
+    });
+
+    describe("getSchool method", () => {
+        it("should return the intern's GitHub username", () => {
+            // Act
+            const obj = new Intern("Dorrian", 12345, "dorrianweber@gmail.com", "UC Davis");
+
+            // Assert
+            expect(obj.getSchool()).toEqual("UC Davis");
+        });
+    });
+
+    describe("getRole method", () => {
+        it("should return 'Intern'", () => {
+            // Act
+            const obj = new Intern("Dorrian", 12345, "dorrianweber@gmail.com", "UC Davis");
+
+            // Assert
+            expect(obj.getRole()).toEqual("Intern");
+        });
+    });
+});
